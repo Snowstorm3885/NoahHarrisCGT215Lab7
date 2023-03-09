72ffa2a23e2af8fa9c7dfbdc5e612b27656c6388
Noah Harris <harr1019@purdue.edu>
2023-03-09 14:10:05 -0500

    Add project files.

---
+++ NoahHarrisCGT215Lab7.cpp
@@ -0,0 +1,112 @@
+// NoahHarrisCGT215Lab7.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFPhysics.h>
+
+using namespace std;
+using namespace sf;
+using namespace sfp;
+
+int main()
+{
+	// Create our window and world with gravity 0,1
+	RenderWindow window(VideoMode(800, 600), "Bounce");
+	World world(Vector2f(0, 1));
+
+	// Create the ball
+	PhysicsCircle ball;
+	ball.setCenter(Vector2f(400, 100));
+	ball.setRadius(20);
+	world.AddPhysicsBody(ball);
+
+	// Create the floor
+	PhysicsRectangle floor;
+	floor.setSize(Vector2f(800, 20));
+	floor.setCenter(Vector2f(400, 590));
+	floor.setStatic(true);
+	world.AddPhysicsBody(floor);
+	int thudCount(0);
+	floor.onCollision = [&thudCount](PhysicsBodyCollisionResult result) {
+		cout << "thud " << thudCount << endl;
+		thudCount++;
+	};
+
+	PhysicsRectangle lWall;
+	lWall.setSize(Vector2f(20, 560));
+	lWall.setCenter(Vector2f(10, 300));
+	lWall.setStatic(true);
+	world.AddPhysicsBody(lWall);
+	lWall.onCollision = [&thudCount](PhysicsBodyCollisionResult result) {
+		cout << "thud " << thudCount << endl;
+		thudCount++;
+	};
+
+	PhysicsRectangle rWall;
+	rWall.setSize(Vector2f(20, 560));
+	rWall.setCenter(Vector2f(790, 300));
+	rWall.setStatic(true);
+	world.AddPhysicsBody(rWall);
+	rWall.onCollision = [&thudCount](PhysicsBodyCollisionResult result) {
+		cout << "thud " << thudCount << endl;
+		thudCount++;
+	};
+
+	PhysicsRectangle roof;
+	roof.setSize(Vector2f(800, 20));
+	roof.setCenter(Vector2f(400, 10));
+	roof.setStatic(true);
+	world.AddPhysicsBody(roof);
+	roof.onCollision = [&thudCount](PhysicsBodyCollisionResult result) {
+		cout << "thud " << thudCount << endl;
+		thudCount++;
+	};
+
+	PhysicsRectangle center;
+	center.setSize(Vector2f(75, 75));
+	center.setCenter(Vector2f(400, 300));
+	center.setStatic(true);
+	world.AddPhysicsBody(center);
+	int bangCount(0);
+	center.onCollision = [&bangCount](PhysicsBodyCollisionResult result) {
+		cout << "bang " << bangCount << endl;
+		bangCount++;
+		if (bangCount == 3) {
+			exit(0);
+		}
+	};
+
+	Clock clock;
+	Time lastTime(clock.getElapsedTime());
+	while (true) {
+		// calculate MS since last frame
+		Time currentTime(clock.getElapsedTime());
+		Time deltaTime(currentTime - lastTime);
+		int deltaTimeMS(deltaTime.asMilliseconds());
+		if (deltaTimeMS > 0) {
+			world.UpdatePhysics(deltaTimeMS);
+			lastTime = currentTime;
+		}
+		window.clear(Color(0, 0, 0));
+		window.draw(ball);
+		window.draw(floor);
+		window.draw(lWall);
+		window.draw(rWall);
+		window.draw(roof);
+		window.draw(center);
+		window.display();
+	}
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
